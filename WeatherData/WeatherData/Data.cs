93693491609c82cfdb84bcce44543a677bf1ac06
maryyj <maria.holster@outlook.com>
2023-02-07 10:11:48 +0100

    if rules, new list

--- WeatherData/WeatherData/Data.cs
+++ WeatherData/WeatherData/Data.cs
@@ -13,34 +13,22 @@ namespace WeatherData
         public int Year { get; set; }
         public int Month { get; set; }
         public int Day { get; set; }
-        //time-props?
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
         public string InOrOut { get; set; }
         public double Temp { get; set; }
         public int Humidity { get; set; }
 
-        public static List<Data> WeatherData { get; set; } = new List<Data>();
+        public static List<Data> WeatherDataInside { get; set; } = new List<Data>();
+        public static List<Data> WeatherDataOutside { get; set; } = new List<Data>();
 
 
         public static void ReadFile()
         {
             string[] weatherData = File.ReadAllLines("../../../TempData/tempdata5-med fel.txt");
-            //string text = "2016-06-01";
-            //string pattern = "(\\d{4})-(?<month>\\d{2})-(\\d{2}).+,(?<inOrOut>\\w{3,4}),(?<temp>\\d{1,2}\\.\\d),(?<hum>\\d{2})";
-            //List<string> validData = new();
-            //foreach (Match m in Regex.Matches(weatherData, pattern).Cast<Match>())
-            //{
-            //    if (m.Groups.Count > 0)
-            //    {
-            //        foreach (Group g in m.Groups)
-            //        {
-            //            //if(g.Name.Equals(1) && g.Value.Equals(2016))
-            //            Console.WriteLine(g.Name + " : " + g.Value);
-
-            //        }
-            //    }
-            //}
             //WeatherData.RemoveRange(0, WeatherData.Count());
-            Regex regex = new("(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2}).+,(?<inOrOut>\\w{3,4}),(?<temp>\\d{1,2}\\.\\d),(?<hum>\\d{2})");
+            Regex regex = new("(?<year>\\d{4})-(?<month>\\d{2})-(?<day>\\d{2}) (?<hour>[0-2][0-9]):(?<min>[0-5][0-9]):(?<sec>[0-5][0-9]),(?<inOrOut>\\w{3,4}),(?<temp>\\-?\\d{1,2}\\.\\d),(?<hum>\\d{2})");
             foreach (var line in weatherData)
             {
                 Match match = regex.Match(line);
@@ -49,29 +37,59 @@ namespace WeatherData
                     int year = int.Parse(match.Groups["year"].Value);
                     int month = int.Parse(match.Groups["month"].Value);
                     int day = int.Parse(match.Groups["day"].Value);
+                    int hour = int.Parse(match.Groups["hour"].Value);
+                    int min = int.Parse(match.Groups["min"].Value);
+                    int sec = int.Parse(match.Groups["sec"].Value);
                     string inOrOut = match.Groups["inOrOut"].Value;
                     double temp = double.Parse(match.Groups["temp"].Value, System.Globalization.CultureInfo.InvariantCulture);
                     int hum = int.Parse(match.Groups["hum"].Value);
 
-                    if (year == 2016 && month > 05)
+                    if (year == 2016 && month > 05 && month <= 12 && day > 0 && day <= 31 && hour < 24)
                     {
-                        WeatherData.Add(new Data()
+                        if (inOrOut == "Inne" || inOrOut == "inne" && temp > 16 && temp < 30 && hum > 10 && hum <= 100)
                         {
-                            Year = year,
-                            Month = month,
-                            Day = day,
-                            InOrOut = inOrOut,
-                            Temp = temp,
-                            Humidity = hum
-                        });
+                            WeatherDataInside.Add(new Data()
+                            {
+                                Year = year,
+                                Month = month,
+                                Day = day,
+                                Hour = hour,
+                                Minute= min,
+                                Second= sec,
+                                InOrOut = inOrOut,
+                                Temp = temp,
+                                Humidity = hum
+                            });
+                        }
+                        else if(inOrOut == "Ute" || inOrOut == "ute" && temp > -35 && temp < 40 && hum > 10 && hum <= 100)
+                        {
+                            WeatherDataOutside.Add(new Data()
+                            {
+                                Year = year,
+                                Month = month,
+                                Day = day,
+                                Hour = hour,
+                                Minute = min,
+                                Second = sec,
+                                InOrOut = inOrOut,
+                                Temp = temp,
+                                Humidity = hum
+                            });
+                        }
                     }
                 }
             }
-            foreach (var w in WeatherData)
+            foreach (var w in WeatherDataInside)
             {
-                Console.WriteLine(w.Year + " " + w.Month + " " + w.Day + " " + w.InOrOut + " " + w.Temp + " " + w.Humidity);
+                Console.WriteLine(w.Year + " " + w.Month + " " + w.Day + " " + w.Hour + " " + w.Minute + " " + w.Second + " " + w.InOrOut + " " + w.Temp + " " + w.Humidity);
+
+            }
+            foreach (var w in WeatherDataOutside)
+            {
+                Console.WriteLine(w.Year + " " + w.Month + " " + w.Day + " " + w.Hour + " " + w.Minute + " " + w.Second + " " + w.InOrOut + " " + w.Temp + " " + w.Humidity);
                 Console.ReadKey();
             }
+            
         }
         public static List<string> GetMonth(string expression, string inOrOut)
         {
